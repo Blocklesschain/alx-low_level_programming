bd206fa119fc69cc2ebae21b41d3132b8a0d86fd
Blocklesschain <blocklesschain@gmail.com>
2023-02-16 20:49:13 -0800

    git

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/**
- *  * main - this is the main function
- *   *
- *    * Return: 0 when successful
- *     */
+/*
+ * main - this is the main function
+ * Return: 0 when successful*    
+ */
 
 int main(void)
 {
