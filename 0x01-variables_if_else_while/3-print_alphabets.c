d309f8a70b91dc582c34c7e8b84ae9dbd9e4f1c3
Blocklesschain <Blocklesschain@gmail.com>
2023-02-17 21:11:49 -0800

    alphabets revisited

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  * main - Entry point
- *   * Description: checks on the last digit of the assigned var
- *    * Return: Always 0 (success)
+ * main - Entry point
+ * Description: checks on the last digit of the assigned var
+ * Return: Always 0 (success)
  */
 int main(void)
 {
