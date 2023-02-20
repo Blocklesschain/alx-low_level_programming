9af9c0d1f227dfc6f62e066224db27bc95abf9ea
Blocklesschain <Blocklesschain@gmail.com>
2023-02-19 22:41:33 -0800

    code restructured with curled brackets removed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+/* betty style doc for function main goes there */
 /**
- *  * main - entry point
- *   * Description: prints  if a number is positive or negative.
- *    * Return: 0
+ * main - entry point
+ * Description: prints  if a number is positive or negative.
+ * Return: 0
  */
 int main(void)
 {
@@ -13,18 +15,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-{
-	printf("%d is positive\n", n);
-}
+		printf("%d is positive", n);
 	else if (n == 0)
-{
-	printf("%d is zero\n", n);
-}
-	else if (n < 0)
-{
-
-	printf("%d is negative\n", n);
-}
+		printf("%d is zero", n);
+	else
+		printf("%d is negative", n);
+	printf("\n");
 	return (0);
 }
 
