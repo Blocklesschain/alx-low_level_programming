c35e12f4a36b06874d60ab6edc2324b891964bd5
Blocklesschain <Blocklesschain@gmail.com>
2023-02-19 23:25:30 -0800

    restrucutred the code, removing curled brackets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,12 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-	if (m > 5)
+	if
+		(m > 5)
 		printf("Last digit of %d is %d and is greater than 5", n, m);
-	else if (m == 0)
+	else if
+		(m == 0)
 		printf("Last digit of %d is %d and is zero", n, m);
-	else if (m < 6 && m != 0)
+	else
+		(m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
+
 	printf("\n");
 
 	return (0);
