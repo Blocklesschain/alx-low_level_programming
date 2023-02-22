f145d473c1a850b49fbba3c86ae35029243b728c
Blocklesschain <Blocklesschain@gmail.com>
2023-02-21 17:25:51 -0800

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - function that prints 10x alphabets
+ * 
+ * Return: always 0
  */
 
 void print_alphabet_x10(void)
