8a4ffc261525ffdca2e3e956df149d0a8a257746
Blocklesschain <Blocklesschain@gmail.com>
2023-03-02 02:08:26 -0800

    .

--- 0x06-pointers_arrays_strings/5-main.c
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -10,8 +10,8 @@ int main(void)
 	char str[] = "Look up!\n";
 	char *ptr;
 
-		ptr = string_toupper(str);
-		printf("%s", ptr);
-		printf("%s", str);
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
 	return (0);
 }
